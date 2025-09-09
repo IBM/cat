6f730e1fbde6b8b3ed21035bbdcc713d844ba3d1
Cássia Sampaio <4005687+cassiasamp@users.noreply.github.com>
2025-09-09 20:39:56 -0300

    removing extra space from inference.py

--- src/common/inference.py
+++ src/common/inference.py
@@ -5,7 +5,6 @@ class InferenceEngine:
     def __init__(self, model_id, max_tokens):
         self.model_id = model_id
         self.max_tokens = max_tokens 
-
     def __call__(self):
         pass
 
@@ -16,8 +15,7 @@ class HFInferenceEngine(InferenceEngine):
         self.model = AutoModelForCausalLM.from_pretrained(model_id, device_map="auto")
         self.pipe = pipeline("text-generation", model=self.model, tokenizer=self.tokenizer)
         self.do_sample = do_sample
-        self.temperature = temperature
-        
+        self.temperature = temperature        
     def __call__(self, prompts):
         outputs = self.pipe(prompts, max_new_tokens=self.max_tokens, do_sample=self.do_sample, temperature=self.temperature)
         return [ output[0]["generated_text"][len(prompt):].strip() for output, prompt in zip(outputs, prompts) ]
