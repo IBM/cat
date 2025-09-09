bb224b5e7d18a149ba4524ff1a925f3fc82a1056
Cássia Sampaio <4005687+cassiasamp@users.noreply.github.com>
2025-09-09 20:37:55 -0300

    removing comments from compute_MCQA.py

--- src/output_analysis/compute_MCQA.py
+++ src/output_analysis/compute_MCQA.py
@@ -3,20 +3,12 @@ import pandas as pd
 import json
 import numpy as np
 from tqdm import tqdm
-
 from .. import ourlib
 
 def main():
-    """Reads a filename from a command-line argument using argparse.
-
-    Example usage:
-    python your_script.py --input my_file.xlsx
-    """
-
     parser = argparse.ArgumentParser(description="Generate prompts given an input file in XLSX or CSV format.")
     parser.add_argument("-i", required=True, help="Name of the input file")
     args = parser.parse_args()
-
 
     input_file = args.i
     print(f"The provided input file is: {input_file}")
@@ -33,12 +25,10 @@ def main():
             
         if evaluation_data['model_choice'] == evaluation_data['correct_choice']:
             totals['correct'] += 1
-
         totals['count'] += 1
 
     print(f"MCQA Accuracy:")
     print(f"   {totals['correct']/totals['count']:.3F}")
-
 
 if __name__ == "__main__":
   main()
