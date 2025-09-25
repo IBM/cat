4a86ee5e628aa3e622456e8e97e2af75b32127b0
Cássia Sampaio <4005687+cassiasamp@users.noreply.github.com>
2025-09-25 16:50:24 -0300

    update in compute_MV.py

--- src/output_analysis/compute_MV.py
+++ src/output_analysis/compute_MV.py
@@ -3,7 +3,8 @@ import pandas as pd
 import json
 import numpy as np
 from tqdm import tqdm
-from .. import ourlib
+#from .. import ourlib
+
 
 def get_most_voted_response(votes):
     max_votes = -1
@@ -14,10 +15,18 @@ def get_most_voted_response(votes):
             max_voted = vote
     return max_voted    
 
+def find_key_by_value(dictionary, value):
+    for key, val in dictionary.items():
+        if val == value:
+            return key
+    return None  # If value is not found in the dictionary
+
+
 def main():
     parser = argparse.ArgumentParser(description="Generate prompts given an input file in XLSX or CSV format.")
     parser.add_argument("-i", required=True, help="Name of the input file")
-    parser.add_argument("-f", required=False, help="Filter variations, separed by commas. Ex: standard_MC,standard_MC_shuffled", default=None)
+    parser.add_argument("-m", required=True, help="Name of model")
+    parser.add_argument("-f", required=False, help="Filter variations. Ex: standard_MC_shuffled", default=None)
     args = parser.parse_args()
 
     input_file = args.i
@@ -28,13 +37,16 @@ def main():
     else:
         to_filter = None
 
+    model =  args.m
+    totals = {}
+    evaluation_types = to_filter#[args.f]
+
     input_data = pd.read_excel(input_file)
 
-    total_correct = 0
-    count = 0
-    votes = {}
-    corrects = 0
-    incorrects = 0
+    totals[model] = { 
+        'count': 0,
+        'correct_mv': 0,   
+    }
 
     for idx, row in tqdm(input_data.iterrows(), total=len(input_data)):   
         try:
@@ -44,32 +56,60 @@ def main():
             model_choice = evaluation_data['standard_MC'][0]['model_choice']
         except json.JSONDecodeError as e:
             print("Invalid JSON data:", e)
-        
-        for evaluation_type in evaluation_data:
-            if to_filter is None or evaluation_type in to_filter:
-                curr_evaluation_data = evaluation_data[evaluation_type]
-                for evaluation_element in curr_evaluation_data:
-                    alt_model_choice = evaluation_element['model_choice']
-                    alt_model_answer = evaluation_element['choices'][alt_model_choice]
-                    choice_vote = ourlib.find_key_by_value(choices, alt_model_answer)
-                    if choice_vote not in votes:
-                        votes[choice_vote] = 0
-                    votes[choice_vote] += 1
-                    if evaluation_element['model_choice'] == evaluation_element['correct_choice']:
-                        corrects += 1
-                    else:
-                        incorrects += 1
-        if corrects > incorrects:
-            total_correct += 1                
-        count += 1
 
-        max_voted = get_most_voted_response(votes)  
+        votes = {}
+        local_total = {'correct': 0, 'incorrect': 0, 'count': 0}
+        eval_idx=0
+
+        for evaluation_type in evaluation_types:
+            for ridx, decoupled_response in enumerate(evaluation_data[evaluation_type]):
+                evaluation_signature = f"{evaluation_type}_{ridx}"
+
+                if 'model_choice' not in decoupled_response:
+                    print(decoupled_response)
+                    continue
+                
+                alt_model_choice = decoupled_response['model_choice']
+                if alt_model_choice in decoupled_response['choices']:
+                    alt_model_answer = decoupled_response['choices'][alt_model_choice]
+                else:
+                    alt_model_answer = None # provided choice is not available
+                    continue # just skip
+
+                alt_correct_choice = decoupled_response['correct_choice']
+                if alt_model_choice == alt_correct_choice:
+                    local_total['correct'] += 1
+                else:
+                    local_total['incorrect'] += 1
+
+                local_total['count'] += 1
+
+                choice_vote = find_key_by_value(choices, alt_model_answer)
+                if choice_vote not in votes:
+                    votes[ choice_vote ] = 0
+                votes[ choice_vote ] += 1
+        totals[model]['count'] += 1
+
+        if local_total['count'] == 0: # garbage outputs, just skip
+            continue
+
+        max_voted = get_most_voted_response(votes)
+            
         if max_voted == correct_choice:
-            correct_mv += 1             
+            totals[model]['correct_mv'] += 1
+        
+    data_dict = {
+        'model': [],
+        'MV': []}
 
-    print(f"MV Accuracy:")
-    MV = correct_mv/count
-    print(f"   {MV:.3F}")
+    for model, results in totals.items():
+        data_dict['model'].append(model)
+
+        MV = results['correct_mv']/results['count']
+        print(f"  MV accuracy: {MV:.6F}")
+        data_dict['MV'].append(MV)
+    
 
 if __name__ == "__main__":
   main()
+ 
